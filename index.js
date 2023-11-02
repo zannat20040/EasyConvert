3cddb405f42f4fe618b10158700e57281a55d06c
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-11-02 15:45:52 +0600

    conversion functionality added

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+document.getElementById('convert-btn').addEventListener('click', () => {
+
+    // find - from which , user want to convert
+    const convertFrom = document.getElementById('convert-from').value
+    // find - to which , user want to convert
+    const convertTo = document.getElementById('convert-to').value
+    // get user inputted number
+    const fromValue = document.getElementById('from-value').value
+    // convert the inputed value into decimal
+
+
+    // render condition for hexa conversion
+    if (convertFrom === 'Hexa') {
+        const decimalValue = parseInt(fromValue, 16)
+        handleAllConvert(convertTo, decimalValue)
+    }
+    // render condition for decimal conversion
+    else if (convertFrom === 'Decimal') {
+        const decimalValue = parseInt(fromValue, 10)
+        handleAllConvert(convertTo, decimalValue)
+    }
+    // render condition for binary conversion
+    else if (convertFrom === 'Binary') {
+        const decimalValue = parseInt(fromValue, 2)
+        handleAllConvert(convertTo, decimalValue)
+    }
+    // render condition for octa conversion
+    else if (convertFrom === 'Octa') {
+        const decimalValue = parseInt(fromValue, 8)
+        handleAllConvert(convertTo, decimalValue)
+    }
+
+
+
+})
+
+// function for hexa conversion
+const handleAllConvert = (convertTo, decimalValue) => {
+    if (convertTo === 'Decimal') {
+        document.getElementById('to-value').value = decimalValue
+    }
+    else if (convertTo === 'Binary') {
+        const binaryValue = decimalValue.toString(2)
+        document.getElementById('to-value').value = binaryValue
+    }
+    else if (convertTo === 'Octa') {
+        const octaValue = decimalValue.toString(8)
+        document.getElementById('to-value').value = octaValue
+    }
+    else if (convertTo === 'Hexa') {
+        const hexaValue = decimalValue.toString(16)
+        console.log("Decimal to hexa:", hexaValue);
+        document.getElementById('to-value').value = hexaValue
+    }
+    else {
+        console.log('same type conversion')
+    }
+}
+
